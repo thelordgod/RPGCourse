953829ca1f6d8114ab5286c1c0ef630be2f7f1ff
theonelordgod <george88512@gmail.com>
2023-02-19 23:05:59 +0200

    weapon pickup respawn

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Combat
@@ -5,15 +7,45 @@ namespace Combat
     public class WeaponPickup : MonoBehaviour
     {
         [SerializeField] private Weapon weapon;
-        
+        [SerializeField] private float respawnTime = 5;
+
+        private SphereCollider _collider;
+        private MeshRenderer _meshRenderer;
+        private ParticleSystem.EmissionModule _particleSystemEmission;
+
+        private void Awake()
+        {
+            _collider = GetComponent<SphereCollider>();
+            _meshRenderer = GetComponentInChildren<MeshRenderer>();
+            _particleSystemEmission = GetComponentInChildren<ParticleSystem>().emission;
+        }
+
         private void OnTriggerEnter(Collider other)
         {
-            if (other.CompareTag("Player"))
-            {
-                var player = other.GetComponent<Fighter>();
-                player.EquipWeapon(weapon);
-                Destroy(gameObject);
-            }
+            if (!other.CompareTag("Player")) return;
+            other.GetComponent<Fighter>().EquipWeapon(weapon);
+            StartCoroutine(HideForSeconds(respawnTime));
+        }
+
+        private IEnumerator HideForSeconds(float seconds)
+        {
+            HidePickup();
+            yield return new WaitForSeconds(seconds);
+            ShowPickup();
+        }
+
+        private void ShowPickup()
+        {
+            _collider.enabled = true;
+            _meshRenderer.enabled = true;
+            _particleSystemEmission.enabled = true;
+        }
+
+        private void HidePickup()
+        {
+            _collider.enabled = false;
+            _meshRenderer.enabled = false;
+            _particleSystemEmission.enabled = false;
         }
     }
 }
