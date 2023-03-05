ef1143fd42278ccc613db9d8bd6feb5daf68303e
theonelordgod <george88512@gmail.com>
2023-03-05 19:32:11 +0200

    xp saving

--- Assets/Scripts/Attributes/Experience.cs
+++ Assets/Scripts/Attributes/Experience.cs
@@ -1,14 +1,25 @@
+using Saving;
 using UnityEngine;
 
 namespace Attributes
 {
-    public class Experience : MonoBehaviour
+    public class Experience : MonoBehaviour, ISaveable
     {
         [SerializeField] private float experiencePoints = 0;
 
         public void GainExperience(float experience)
         {
             experiencePoints += experience;
+        }
+
+        public object CaptureState()
+        {
+            return experiencePoints;
+        }
+
+        public void RestoreState(object state)
+        {
+            experiencePoints = (float) state;
         }
     }
 }
